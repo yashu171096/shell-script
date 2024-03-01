98f3ddfe18631cd521c3c6fb72fee501dd1432ab
yashwanth <yashwanth24710@gmail.com>
2024-03-01 20:50:59 +0530

    added condition

--- mail.sh
+++ mail.sh
@@ -2,10 +2,10 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
