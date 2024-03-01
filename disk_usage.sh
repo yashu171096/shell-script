dde2f8326df796bfc4f457fdc1b0b80ee46e4c9b
yashwanth <yashwanth24710@gmail.com>
2024-03-01 19:35:39 +0530

    added condition

---
+++ disk_usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME.$DATE.log
+R=".\e[31m"
+G=".\e[32m"
+N=".\e[0m"
+Y=".\e[33m"
+message=""
+
+DISK_USAGE=$(df -hT | grep -vE "tmpfs|Filesystem")
+DISK_USAGE_THRESHOLD=1
+
+while IFS= read line
+do
+    USAGE=$(echo $line awk '{print $6}' | cut -d % -f1)
+    PARTITION=$(echo $line awk '{print $1}')
+    if [ USAGE -gt DISK_USAGE_THRESHOLD ];
+    then
+        message+="HIGH DISK USAGE ON $PARTITION: $USAGE
+    fi
+done <<< $DISK_USAGE
+
+sh mail.sh yashu24710@gmail.com "HIGH DISK USAGE" "$message" "devops team" "MORE DISK USAGE"
---
+++ mail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TO_ADDRESS=$1
+SUBJECT=$2
+BODY=$3
+TEAM_NAME=$4
+ALERT_TYPE=$5
+
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
