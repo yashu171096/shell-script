553a09abdba4e761d6968a89a59449707ba5bce5
yashwanth <yashwanth24710@gmail.com>
2024-03-01 12:01:40 +0530

    added condition

--- delete-log-files.sh
+++ delete-log-files.sh
@@ -8,3 +8,9 @@ LOGFILE=/tmp/$SCRIPT_NAME.$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +28)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+    echo "deleteing $line" &>>$LOGFILE
+    rm -rf $line
+done << "$FILES_TO_DELETE"    
