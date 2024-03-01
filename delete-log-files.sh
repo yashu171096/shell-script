c5a8d4381bdbe7fd397016b0e49974d7f61fcba7
yashwanth <yashwanth24710@gmail.com>
2024-03-01 11:52:27 +0530

    added condition

---
+++ delete-log-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME.$DATE.log
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +28)
+
+echo "$FILES_TO_DELETE"
