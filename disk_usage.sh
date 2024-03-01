8419882e6bb066546cf7a5d1cda64501c79daa55
yashwanth <yashwanth24710@gmail.com>
2024-03-01 19:40:55 +0530

    added condition

--- disk_usage.sh
+++ disk_usage.sh
@@ -16,7 +16,7 @@ while IFS= read line
 do
     USAGE=$(echo $line awk '{print $6}' | cut -d % -f1)
     PARTITION=$(echo $line awk '{print $1}')
-    if [ USAGE -gt DISK_USAGE_THRESHOLD ];
+    if [ $USAGE -gt $DISK_USAGE_THRESHOLD ];
     then
         message+="HIGH DISK USAGE" ON $PARTITION: $USAGE
     fi
